e4deea67eba481fa74ce150c4e981310ebf5f2eb
Hunter Arthur Gabbard <hunter.gabbard@dgx-1.dcs.ligo-wa.caltech.edu>
2018-01-10 06:51:03 -0800

    not sure what was changed here

--- bin/qtest.py
+++ bin/qtest.py
@@ -1,10 +1,11 @@
 from keras.applications.mobilenet import MobileNet
 import pycbc.psd
 import gmind.generator
+import sys
 
 p = pycbc.psd.aLIGOZeroDetHighPower(2**19, 1.0/16, 15)
 s = gmind.generator.GaussianNoiseGenerator(16, 1024, p, 20)
-p = gmind.generator.WFParamGenerator(["examples/test.ini"])
+p = gmind.generator.WFParamGenerator(["../examples/test.ini"])
 w = gmind.generator.GaussianSignalQImageGenerator(s, p, 3, (224, 224), q=20)
 
 i, t = w.next()
